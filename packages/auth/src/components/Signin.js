98d71b0e822748a04546fc09c4bb7da5c2d42c86
jorge.cedillo <jorge.cedillozamora@notified.com>
2023-07-07 17:28:14 -0600

    First Comit

---
+++ packages/auth/src/components/Signin.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Link } from 'react-router-dom';
+
+function Copyright() {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" to="/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
+
+const useStyles = makeStyles((theme) => ({
+	'@global': {
+		a: {
+			textDecoration: 'none',
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+	card: {
+		height: 150,
+		width: 150,
+		boxShadow: '0px 0px',
+	},
+}));
+
+export default function SignIn({ onSignIn }) {
+	const classes = useStyles();
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<div className={classes.paper}>
+				<Card className={classes.card}>
+					<CardMedia className={classes.card} image="https://i.ibb.co/w6ZyBsR/conentrix-img.png" title="title-sign" />
+				</Card>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<form onSubmit={(e) => e.preventDefault()} className={classes.form} noValidate>
+					<TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+					<TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+					<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+					<Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} onClick={onSignIn}>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link to="/auth/signup">{"Don't have an account? Sign Up"}</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<Box mt={8}>
+				<Copyright />
+			</Box>
+		</Container>
+	);
+}
